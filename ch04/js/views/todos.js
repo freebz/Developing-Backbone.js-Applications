40ac90dc0e5b8aea51d8a4128826103d25039da7
FreeBz <freebz@hananet.net>
2013-08-16 10:00:32 +0900

    Completing and Deleting Todos

--- ch04/js/views/todos.js
+++ ch04/js/views/todos.js
@@ -12,19 +12,43 @@ app.TodoView = Backbone.View.extend({
     template: _.template( $('#item-template').html() ),
 
     events: {
+	'click .toggle': 'togglecompleted',
 	'dbclick label': 'edit',
+	'click .destroy': 'clear',
 	'keypress .edit': 'updateOnEnter',
 	'blur .edit': 'close'
     },
 
     initialize: function() {
 	this.listenTo(this.model, 'change', this.render);
+	this.listenTo(this.model, 'destroy', this.remove);
+	this.listenTo(this.model, 'visible', this.toggleVisible);
     },
 
     render: function() {
 	this.$el.html( this.template( this.model.toJSON() ) );
+
+	this.$el.toggleClass( 'completed', this.model.get('completed') );
+	this.toggleVisible();
+
 	this.$input = this.$('.edit');
 	return this;
+    },
+
+    toggleVisible : function () {
+	this.$el.toggleClass( 'hidden', this.isHidden());
+    },
+
+    isHidden : function () {
+	var isCompleted = this.model.get('completed');
+	return (
+	    (!isCompleted && app.TodoFilter === 'completed')
+	    || (isCompleted && app.TodoFilter === 'active')
+	);
+    },
+
+    togglecompleted: function() {
+	this.model.toggle();
     },
 
     edit: function() {
@@ -46,5 +70,9 @@ app.TodoView = Backbone.View.extend({
 	if ( e.which === ENTER_KEY ) {
 	    this.close();
 	}
+    },
+
+    clear: function() {
+	this.model.destroy();
     }
 });
