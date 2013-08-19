1660a7745b46bb71d09260b821f133eea52b0679
FreeBz <freebz@hananet.net>
2013-08-19 17:54:38 +0900

    Talking to the Server

--- ch05/site/js/app.js
+++ ch05/site/js/app.js
@@ -3,18 +3,6 @@
 var app = app || {};
 
 $(function() {
-    var books = [
-	{ title: 'JavaScript: The Good Parts', author: 'Douglas Crockford',
-	  releaseDate: '2008', keywords: 'JavaScript Programming' },
-	{ title: 'The Little Book on CoffeeScript', author: 'Alex MacCaw',
-	  releaseDate: '2012', keywords: 'CoffeeScript Programming' },
-	{ title: 'Scala for the Impatient', author: 'Cay S. Horstmann',
-	  releaseDate: '1991', keywords: 'Scala Programming' },
-	{ title: 'American Psycho', author: 'Bret Eaoton Ellis',
-	  releaseDate: '1991', keywords: 'Novel Splatter' },
-	{ title: 'Eloquent JavaScript', author: 'Marijn Haverbeke',
-	  releaseDate: '2011', keywords: 'JavaScript Pragramming' }
-    ];
-
-    new app.LibraryView( books );
+    $( '#releaseDate' ).datepicker();
+    new app.LibraryView();
 });
--- ch05/site/js/views/library.js
+++ ch05/site/js/views/library.js
@@ -5,11 +5,13 @@ var app = app || {};
 app.LibraryView = Backbone.View.extend({
     el: '#books',
 
-    initialize: function( initialBooks ) {
-	this.collection = new app.Library( initialBooks );
+    initialize: function() {
+	this.collection = new app.Library();
+	this.collection.fetch({reset: true});
 	this.render();
 
 	this.listenTo( this.collection, 'add', this.renderBook );
+	this.listenTo( this.collection, 'reset', this.render );
     },
 
     events: {
@@ -22,12 +24,25 @@ app.LibraryView = Backbone.View.extend({
 	var formData = {};
 
 	$( '#addBook div' ).children( 'input' ).each( function( i, el ) {
-	    if( $( el ).val() != '' ) {
-		formData[ el.id ] = $( el ).val();
+	    if( $( el ).val() != '' )
+	    {
+		if( el.id === 'keywords' ) {
+		    formData[ el.id ] = [];
+		    _.each( $( el ).val().split( ' ' ), function( keyword ) {
+			formData[ el.id ].push({ 'keyword': keyword });
+		    });
+		} else if( el.id === 'releaseDate' ) {
+		    formData[ el.id ] = $( '#releaseDate' ).datepicker( 'getDate' ).getTime();
+		} else {
+		    formData[ el.id ] = $( el ).val();
+		}
 	    }
+	    // Clear input field value
+	    $( el ).val('');
 	});
 
-	this.collection.add( new app.Book( formData ) );
+//	this.collection.add( new app.Book( formData ) );
+	this.collection.create( formData );
     },
 
     render: function() {
