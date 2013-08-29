ef35e4bb765c955160929cf5f68f0e650d869810
FreeBz <freebz@hananet.net>
2013-08-29 10:49:47 +0900

    Spying on Existing Functions

--- ch15/tests.js
+++ ch15/tests.js
@@ -11,3 +11,13 @@ test('should call all subscribers for a message exactly once', function () {
 
     ok( spy.calledOnce, 'the subscriber was called once' );
 });
+
+test( 'should inspect the jQuery.getJSON usage of jQuery.ajax', function () {
+    sinon.spy( jQuery, 'ajax' );
+
+    jQuery.getJSON( '/todos/completed' );
+
+    ok( jQuery.ajax.calledOnce );
+    equals( jQuery.ajax.getCall(0).args[0].url, '/todos/completed' );
+    equals( jQuery.ajax.getCall(0).args[0].dataType, 'json' );
+});
